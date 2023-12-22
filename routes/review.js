e07e7d26dbc36f9d26c95abcfebb3088c0bed3e2
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2023-12-22 12:50:48 +0530

    ..

--- routes/review.js
+++ routes/review.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
-const Campground = require('../models/campground');
-const Review = require('../models/review');
-const reviews = require('../controller/reviews');
-const ExpressError = require('../utils/ExpressError');
+const { validateCampground, isLoggedIn, isReviewAuthor } = require('../middleware');
+const { createReview, deleteReview } = require('../controller/reviews');
 const catchAsync = require('../utils/catchAsync');
 
-//router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
-console.log(router.post('/' , isLoggedIn, validateReview,  async function(req, res){
-    reviews.createReview()
+router.post('/', isLoggedIn, validateCampground, catchAsync(createReview))
 
-  }))
-router.post('/' , isLoggedIn, validateReview,  async function(req, res){
-    reviews.createReview
-
-  });
-
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(deleteReview))
 
 module.exports = router;
