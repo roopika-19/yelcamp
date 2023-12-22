f77a35dd9652e887bf69eaadcfdeff4b659084df
roopika-19 <roopikasaxena19@gmail.com>
2023-12-22 17:15:29 +0530

    jnklk

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/users');
 
+
 module.exports.renderRegister = (req, res) => {
     res.render('users/register');
 }
@@ -8,16 +9,15 @@ module.exports.register = async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
-        
         const registeredUser = await User.register(user, password);
         req.login(registeredUser, err => {
             if (err) return next(err);
             req.flash('success', 'Welcome to Yelp Camp!');
             res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
+        });
+    } catch (err) {
+        req.flash('error', err.message);
+        res.redirect('/register'); // Added a leading slash here
     }
 }
 
@@ -26,14 +26,9 @@ module.exports.renderLogin = (req, res) => {
 }
 
 module.exports.login = (req, res) => {
-    req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
+            req.flash('success', 'Welcome back!');
+            res.locals.returnTo 
+            res.redirect(redirectUrl);
+        
 }
 
-module.exports.logout = (req, res) => {
-    req.logout();
-    req.flash('success', "Goodbye!");
-    res.redirect('/campgrounds');
-}
